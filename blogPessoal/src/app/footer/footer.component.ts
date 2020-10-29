4a81a898e1823528031a06e25afa629a4632d434
unknown <santiagoyuri373@gmail.com>
2020-10-29 20:44:36 -0300

    parte  2

--- blogPessoal/src/app/footer/footer.component.ts
+++ blogPessoal/src/app/footer/footer.component.ts
@@ -1,21 +1,21 @@
-
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
 import{faFacebook}from'@fortawesome/free-brands-svg-icons'
 import{faInstagram}from'@fortawesome/free-brands-svg-icons'
 import{faLinkedin}from'@fortawesome/free-brands-svg-icons'
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
-export class HomeComponent implements OnInit {
 
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
 
   faFacebook=faFacebook
   faInstagram=faInstagram
   faLinkedin=faLinkedin
+
   constructor() { }
 
   ngOnInit(): void {
