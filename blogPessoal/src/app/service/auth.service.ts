c24979e79d0aedaba34d93a910ca52b926e24e3d
unknown <santiagoyuri373@gmail.com>
2020-11-29 15:37:49 -0300

    v 1.1

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -11,13 +11,13 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   logar(userLogin: UserLogin){
-    return this.http.post('http://localhost:8080/usuarios/logar',userLogin)
+    return this.http.post('https://desolado-blog-pessoal.herokuapp.com/usuarios/logar',userLogin)
 
 
 
   }
   cadastrar(user: User){
-    return this.http.post('http://localhost:8080/usuarios/cadastrar',user)
+    return this.http.post('https://desolado-blog-pessoal.herokuapp.com/usuarios/cadastrar',user)
     
   }
   btnSair(){
